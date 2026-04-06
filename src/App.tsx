1940656db6a6e7c7952d253a51a1837b023c28c5
Roanbea <roanbeabalitos@gmail.com>
2026-04-06 22:45:21 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navigation />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
